72483b1063fe341cfd578b90202ffc8c3d5fca34
szy39 <eymenylmz39@gmail.com>
2025-09-22 14:52:44 +0300

    created teams

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,15 +1,15 @@
-import { CountriesResponse, LeagueSearchParams, LeaguesResponse, SeasonsResponse } from '../types/types';
+import { CountriesResponse, LeagueSearchParams, LeaguesResponse, SeasonsResponse, TeamSearchParams, TeamsResponse } from '../types/types';
 
 const API_BASE_URL = "https://v1.basketball.api-sports.io";
 const API_KEY = process.env.NEXT_PUBLIC_RAPIDAPI_KEY || "c676c73bee2956cd3998d54d147b0bdc";
 
-// Ortak header'lar
+// header
 const getHeaders = () => ({
     "x-rapidapi-host": "v1.basketball.api-sports.io",
     "x-rapidapi-key": API_KEY
 });
 
-// Ortak fetch fonksiyonu
+// fetch function
 const apiRequest = async <T>(url: string): Promise<T> => {
     const response = await fetch(url, {
         method: "GET",
@@ -55,4 +55,43 @@ export const getAllLeagues = async (params?: LeagueSearchParams): Promise<League
     }
     
     return apiRequest<LeaguesResponse>(url);
+};
+
+export const getTeams = async (params?: TeamSearchParams): Promise<TeamsResponse> => {
+    let url = `${API_BASE_URL}/teams`;
+    const queryParams = new URLSearchParams();
+    
+    if (params?.id) {
+        queryParams.append('id', String(params.id));
+    }
+    
+    if (params?.name) {
+        queryParams.append('name', params.name);
+    }
+    
+    if (params?.country_id) {
+        queryParams.append('country_id', String(params.country_id));
+    }
+    
+    if (params?.country) {
+        queryParams.append('country', params.country);
+    }
+    
+    if (params?.league) {
+        queryParams.append('league', String(params.league));
+    }
+    
+    if (params?.season) {
+        queryParams.append('season', params.season);
+    }
+    
+    if (params?.search) {
+        queryParams.append('search', params.search);
+    }
+    
+    if (queryParams.toString()) {
+        url += `?${queryParams.toString()}`;
+    }
+    
+    return apiRequest<TeamsResponse>(url);
 };
